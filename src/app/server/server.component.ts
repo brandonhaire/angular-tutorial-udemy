f0a3cbfe285601d4ad0c17d4d5e6c0dd08645bef
Haire Brandon <Brandon.Haire@ngc.com>
2023-01-09 15:26:46 -0500

    40. Applying CSS Classes Dynamically with ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .online{
+      color: white;
+     }
+  `]
 })
 export class ServerComponent {
   serverId = 10;
@@ -11,8 +16,8 @@ export class ServerComponent {
   constructor() {
     this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
   }
-  
-  getServerStatus(){
+
+  getServerStatus() {
     return this.serverStatus;
   }
 
