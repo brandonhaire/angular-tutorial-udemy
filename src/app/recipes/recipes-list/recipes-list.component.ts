173caf9cdc1e662ddd162ded581e2a3ba753296f
Haire Brandon <Brandon.Haire@ngc.com>
2023-01-24 14:06:18 -0500

    54.Outputting a List of Recipes with NgFor
    couldn't get any images to display properly

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  imgPath: string  = "https://as1.ftcdn.net/v2/jpg/01/84/04/10/1000_F_184041094_LjaVqs8hHM3sZLvbKl442ogMC528F49R.jpg";
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is simply a test", this.imgPath),
+    new Recipe("A Test Recipe", "This is simply a test", this.imgPath)
+  ];
 
   constructor() { }
 
