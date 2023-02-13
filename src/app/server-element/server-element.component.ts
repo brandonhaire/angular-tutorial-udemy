a2663b5476b8a4ae547d20c73ec0e3b71ad348e9
Haire Brandon <Brandon.Haire@ngc.com>
2023-02-13 15:25:19 -0500

    78.Seeing Lifecycle Hooks in Action

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,17 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewEncapsulation
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +19,52 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element: {type: string, name: string, content: string};
+export class ServerElementComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
+  // @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log("constructor called!");
   }
 
+  ngOnInit(): void {
+    console.log("ngOnInit() called!");
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("ngOnChanges called!");
+    console.log(changes);
+  }
+
+  ngDoCheck(): void {
+    console.log("ngDoCheck() called!");
+  }
+
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit() called!");
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked() called!");
+  }
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit() called!")
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked() called!");
+  }
+
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy() called!");
+  }
 }
