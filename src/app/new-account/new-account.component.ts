141fdfbac769aa6de3807288c420861d34472843
Haire Brandon <Brandon.Haire@ngc.com>
2023-02-28 10:54:35 -0500

    112.Using Services for Cross-Component Communication

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -11,7 +11,11 @@ import { LoggingService } from '../logging.service';
 export class NewAccountComponent {
 
   constructor(private loggingService: LoggingService,
-    private accountsService: AccountsService) { }
+    private accountsService: AccountsService) {
+      this.accountsService.statusUpdated.subscribe(
+        (status: string) => alert('New Status: ' + status)
+      );
+     }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
