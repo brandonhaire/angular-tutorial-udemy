d1588f49713aa9bbe196cfed78a578d185272cbb
Haire Brandon <Brandon.Haire@ngc.com>
2023-03-14 16:03:22 -0400

    120.Using Services for Pushing Data from A to B

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -14,6 +14,11 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit(): void {
     this.ingredients = this.slService.getIngredients();
+    this.slService.ingredientsChanged.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
+    )
   }
 
 }
