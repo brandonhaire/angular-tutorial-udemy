ce86186554c5ed313603c14020d06bdae8787e37
Haire Brandon <Brandon.Haire@ngc.com>
2023-03-16 14:04:38 -0400

    first way of assigning http types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +18,6 @@ export class AppComponent implements OnInit {
   }
 
   onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
     this.http.post(
       'https://udemy-da4cd-default-rtdb.firebaseio.com/posts.json',
       postData
@@ -26,28 +26,24 @@ export class AppComponent implements OnInit {
     });
   }
 
-  onFetchPosts() {
-    // Send Http request
-    this.fetchPosts();
-  }
+  onFetchPosts() { this.fetchPosts(); }
 
-  onClearPosts() {
-    // Send Http request
-  }
+  onClearPosts() { }
 
   private fetchPosts() {
     this.http
       .get('https://udemy-da4cd-default-rtdb.firebaseio.com/posts.json')
       .pipe(
-        map(responseData => {
-          const postsArray = [];
+        map((responseData: { [key: string]: Post }) => {
+          const postsArray: Post[] = [];
           for (const key in responseData) {
             if (responseData.hasOwnProperty(key)) {
               postsArray.push({ ...responseData[key], id: key });
             }
           }
           return postsArray;
-        }))
+        })
+      )
       .subscribe(posts => {
         console.log(posts);
       });
