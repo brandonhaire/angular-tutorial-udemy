7ff15e9c86c6c0fe49e159356bded6b2c4d0c049
Haire Brandon <Brandon.Haire@ngc.com>
2023-03-16 16:25:49 -0400

    261.Using a Service for Http Requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,58 +2,39 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs';
 import { Post } from './post.model';
+import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
   isFetching = false;
 
-  constructor(private http: HttpClient) { }
+  constructor(private postsService: PostsService) { }
 
   ngOnInit() {
-    this.fetchPosts();
+    this.postsService.fetchPosts();
   }
 
   onCreatePost(postData: { title: string; content: string }) {
-    this.http.post<{ name: string }>(
-      'https://udemy-da4cd-default-rtdb.firebaseio.com/posts.json',
-      postData
-    ).subscribe(responseData => {
-      console.log(responseData);
-    });
+    this.postsService.createAndStorePost(postData.title, postData.content);
   }
 
-  onFetchPosts() { this.showPacMan(); this.fetchPosts(); }
+  onFetchPosts() {
+    this.showPacMan();
+    this.postsService.fetchPosts();
+  }
 
-  showPacMan(){
+  showPacMan() {
     this.isFetching = true;
-    setTimeout(() => {  this.isFetching = false;}, 1500);
+    setTimeout(() => {
+      this.isFetching = false;
+    }, 1500);
   }
 
   onClearPosts() { }
 
-  private fetchPosts() {
-    // this.isFetching = true;
-    this.http
-      .get<{ [key: string]: Post }>('https://udemy-da4cd-default-rtdb.firebaseio.com/posts.json')
-      .pipe(
-        map(responseData => {
-          const postsArray: Post[] = [];
-          for (const key in responseData) {
-            if (responseData.hasOwnProperty(key)) {
-              postsArray.push({ ...responseData[key], id: key });
-            }
-          }
-          return postsArray;
-        })
-      )
-      .subscribe(posts => {
-        this.loadedPosts = posts;
-        // this.isFetching = false;
-      });
-  }
 }
---
+++ src/app/posts.service.ts
@@ -0,0 +1,38 @@
+import { Post } from './post.model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { map } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class PostsService {
+
+    constructor(private http: HttpClient) { }
+
+    createAndStorePost(title: string, content: string) {
+        const postData: Post = { title: title, content: content };
+        this.http.post<{ name: string }>(
+            'https://udemy-da4cd-default-rtdb.firebaseio.com/posts.json',
+            postData
+        ).subscribe(responseData => {
+            console.log(responseData);
+        });
+    }
+
+    fetchPosts() {
+        this.http
+            .get<{ [key: string]: Post }>('https://udemy-da4cd-default-rtdb.firebaseio.com/posts.json')
+            .pipe(
+                map(responseData => {
+                    const postsArray: Post[] = [];
+                    for (const key in responseData) {
+                        if (responseData.hasOwnProperty(key)) {
+                            postsArray.push({ ...responseData[key], id: key });
+                        }
+                    }
+                    return postsArray;
+                })
+            )
+            .subscribe(posts => {
+            });
+    }
+}
