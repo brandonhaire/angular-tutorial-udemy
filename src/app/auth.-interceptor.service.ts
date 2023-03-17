55be1bd9aa576a0b1fab7a79662468924675b808
Haire Brandon <Brandon.Haire@ngc.com>
2023-03-17 15:50:17 -0400

    274.Response Interceptors

--- src/app/auth.-interceptor.service.ts
+++ src/app/auth.-interceptor.service.ts
@@ -1,5 +1,5 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Observable, tap } from "rxjs";
 
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
@@ -7,7 +7,16 @@ export class AuthInterceptorService implements HttpInterceptor {
         // if (req.url){}   way to block requests from certian URLs 
         console.log('Request is on its way');
         console.log(req);
-        const modifiedRequest = req.clone({headers: req.headers.append('Auth', 'xyz')})
-        return next.handle(modifiedRequest);
+        const modifiedRequest = req.clone({
+            headers: req.headers.append('Auth', 'xyz')
+        });
+        return next.handle(modifiedRequest).pipe(
+            tap(event => {
+                if (event.type === HttpEventType.Response) {
+                    console.log('Response arrived, body data: ');
+                    console.log(event.body);
+                }
+            })
+        );
     }
 }
