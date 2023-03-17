f4f4111ff5b5b1c8ee2d747316fbca01441b4a93
Haire Brandon <Brandon.Haire@ngc.com>
2023-03-17 15:59:38 -0400

    275.Multiple Interceptors

--- src/app/auth.-interceptor.service.ts
+++ src/app/auth.-interceptor.service.ts
@@ -3,20 +3,10 @@ import { Observable, tap } from "rxjs";
 
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-        //     throw new Error("Method not implemented.");
         // if (req.url){}   way to block requests from certian URLs 
-        console.log('Request is on its way');
-        console.log(req);
         const modifiedRequest = req.clone({
             headers: req.headers.append('Auth', 'xyz')
         });
-        return next.handle(modifiedRequest).pipe(
-            tap(event => {
-                if (event.type === HttpEventType.Response) {
-                    console.log('Response arrived, body data: ');
-                    console.log(event.body);
-                }
-            })
-        );
+        return next.handle(modifiedRequest);
     }
 }
