21bf30c4bbf47ad742ee709d28e073145163a78d
Haire Brandon <Brandon.Haire@ngc.com>
2023-03-20 15:35:20 -0400

    174.Errors & Completion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -22,12 +22,23 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count == 5) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater 3!'));
+        }
         count++;
       }, 1000);
     });
 
    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
       console.log(data);
+    }, error => {
+      console.log(error.message);
+      alert(error);
+    }, () => {
+      console.log('Completed!');
     });
   }
 
