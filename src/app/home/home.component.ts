e6be475d81e6580fd4ae2d9ac0e71f8fab398945
Haire Brandon <Brandon.Haire@ngc.com>
2023-03-20 15:20:12 -0400

    172.Getting Closer to the Core of Observables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { Subscription } from 'rxjs-compat';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    this.firstObsSubscription = interval(1000).subscribe(count => {
+      console.log(count);
+    });
   }
 
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+  }
 }
+
