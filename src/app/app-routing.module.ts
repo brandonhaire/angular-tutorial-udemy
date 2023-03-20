bef54d658e197c606b5a922a1cb1daa9677f6459
Haire Brandon <Brandon.Haire@ngc.com>
2023-03-20 14:46:43 -0400

    152.Understanding Location Strategies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -37,6 +37,7 @@ const appRoutes: Routes = [
 @NgModule({
     imports: [
         RouterModule.forRoot(appRoutes)
+        // RouterModule.forRoot(appRoutes, {useHash: true})
     ],
     exports: [RouterModule]
 })
