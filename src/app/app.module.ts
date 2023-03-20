a757d147f45a5153e4767d4f3b0c7fbc94732ad0
Haire Brandon <Brandon.Haire@ngc.com>
2023-03-20 12:38:43 -0400

    138.Practicing and some Common Gotchas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,8 @@ const appRoutes: Routes = [
   { path: 'users', component: UsersComponent },
   { path: 'users/:id/:name', component: UserComponent },
   { path: 'servers', component: ServersComponent },
-  { path: 'servers/:id/edit', component: EditServerComponent}
+  { path: 'servers/:id/edit', component: EditServerComponent},
+  { path: 'servers/:id', component: ServerComponent}
 ];
 
 @NgModule({
