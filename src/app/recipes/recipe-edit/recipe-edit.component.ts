6fef01fdfeeb00a2988e69ad10c48137df252fc5
Haire Brandon <Brandon.Haire@ngc.com>
2023-03-21 11:35:45 -0400

    231.Adding new Ingredient Controls

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -31,6 +31,15 @@ export class RecipeEditComponent implements OnInit {
     console.log(this.recipeForm);
   }
 
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    );
+  }
+
   get controls() {
     return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
