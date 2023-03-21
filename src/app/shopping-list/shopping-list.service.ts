ab2aad5708b7bb57128e0c86aa3c7f50784fb9bd
Haire Brandon <Brandon.Haire@ngc.com>
2023-03-21 15:02:53 -0400

    failed attempt to make add ingredient function filter out duplicate names

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -2,41 +2,85 @@ import { Subject } from "rxjs";
 import { Ingredient } from "../shared/inredient.model";
 
 export class ShoppingListService {
-    ingredientsChanged = new Subject<Ingredient[]>();
-    startedEditing = new Subject<number>();
-    private ingredients: Ingredient[] = [
-        new Ingredient('Apples', 5),
-        new Ingredient('Tomatoes', 10)
-      ];
+  ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
 
-      getIngredients(): Ingredient[] {
-        return this.ingredients.slice();
-      }
+  getIngredients(): Ingredient[] {
+    return this.ingredients.slice();
+  }
 
-      addIngredient(ingredient: Ingredient) {
-        this.ingredients.push(ingredient);
-        this.ingredientsChanged.next(this.ingredients.slice());
-      }
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
 
-      addIngredients(ingredients: Ingredient[]) {
-        // for (let ingredient of ingredients) {
-        //   this.addIngredient(ingredient);
-        // }
-        this.ingredients.push(...ingredients);
-        this.ingredientsChanged.next(this.ingredients.slice());
-      }
+  addIngredients(ingredients: Ingredient[]) {
+    // for (let ingredient of ingredients) {
+    //   this.addIngredient(ingredient);
+    // }
+    this.ingredients.push(...ingredients);     //equivalent to the code above
+    this.ingredientsChanged.next(this.ingredients.slice());
 
-      getIngredient(index: number) {
-        return this.ingredients[index];
-      }
+    //first attempt
+    // for (let i = 0; i < ingredients.length; i++) {
+    //   console.log("first for loop");
+    //   if (this.ingredients.includes(ingredients[i])) {
+    //     // add amount of ingredient
+    //     let index = 0;
+    //     let amount = 0;
+    //     for (let j = 0; j < this.ingredients.length; j++){
+    //       console.log("second for loop");
+    //       if (this.ingredients[j].name.toLowerCase === ingredients[i].name.toLowerCase) {
+    //         index = j;
+    //         amount = ingredients[i].amount;
+    //       }
+    //     }
+    //     this.ingredients[index].amount += amount;
 
-      updateIngredient(index: number, newIngredient: Ingredient) {
-        this.ingredients[index] = newIngredient;
-        this.ingredientsChanged.next(this.ingredients.slice());
-      }
+    //   } else {
+    //     // add new ingredient (push)
+    //     this.ingredients.push(ingredients[i]);
+    //   }
 
-      deleteIngredient(index: number) {
-        this.ingredients.splice(index, 1);
-        this.ingredientsChanged.next(this.ingredients.slice());
-      }
+    //second attempt
+    // let duplicates: {name: string, amount: number, index: number}[] = [];
+    // for (let i = 0; i < ingredients.length; i++) {
+    //   if (this.ingredients.includes(ingredients[i])) {
+    //     const duplicate = ingredients[i].name;
+    //     for (let j = 0; j < this.ingredients.length; j++){
+    //       if (this.ingredients[j].name.toLowerCase === duplicate.toLowerCase) {
+    //         duplicates.push({name: ingredients[i].name,amount: ingredients[i].amount,index: j});
+    //     }
+    //   }
+    //   } else {
+    //     this.ingredients.push(ingredients[i]);
+    //   }
+    // }
+
+    // for (let i = 0; i< duplicates.length; i++) {
+    //   // const name = duplicates[i].name;
+    //   // const amount = duplicates[i].amount;
+    //   this.ingredients[duplicates[i].index].amount += duplicates[i].amount;
+    // }
+    // console.log(duplicates);
+  }
+
+
+  getIngredient(index: number) {
+    return this.ingredients[index];
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
 }
