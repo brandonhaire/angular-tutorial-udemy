91d1e71a6526de69fbecb16e37ed4211505564c0
Haire Brandon <Brandon.Haire@ngc.com>
2023-03-21 10:11:52 -0400

    222.Updating existing Items

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -29,4 +29,9 @@ export class ShoppingListService {
       getIngredient(index: number) {
         return this.ingredients[index];
       }
+
+      updateIngredient(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+      }
 }
