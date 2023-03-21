62d4ed28c1bc29aa514f76baf8358d999b171934
Haire Brandon <Brandon.Haire@ngc.com>
2023-03-21 15:12:38 -0400

    283.Transforming Response Data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,25 +7,27 @@ import { Recipe } from "./recipe.model";
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
+  private recipes: Recipe[] = [];
 
-  imgPath: string  = "https://as1.ftcdn.net/v2/jpg/01/84/04/10/1000_F_184041094_LjaVqs8hHM3sZLvbKl442ogMC528F49R.jpg";
-  private recipes: Recipe[] = [
-    new Recipe(
-      "Tasty Schnitzel", 
-      "A super-tasty Schnitzel - just awesome!",
-      "https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG", 
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]),
-    new Recipe("Big Fat Burger",
-    "What else you need to say?", 
-    "https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg",
-    [
-      new Ingredient('Buns', 2),
-      new Ingredient('Meat', 1)
-    ])
-  ];
+
+  // imgPath: string  = "https://as1.ftcdn.net/v2/jpg/01/84/04/10/1000_F_184041094_LjaVqs8hHM3sZLvbKl442ogMC528F49R.jpg";
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     "Tasty Schnitzel", 
+  //     "A super-tasty Schnitzel - just awesome!",
+  //     "https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG", 
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('French Fries', 20)
+  //     ]),
+  //   new Recipe("Big Fat Burger",
+  //   "What else you need to say?", 
+  //   "https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg",
+  //   [
+  //     new Ingredient('Buns', 2),
+  //     new Ingredient('Meat', 1)
+  //   ])
+  // ];
 
   constructor(private slService: ShoppingListService) {}
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,31 +1,37 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class DataStorageService {
     private apiURL = 'https://udemy-da4cd-default-rtdb.firebaseio.com/recipes.json';
     constructor(private http: HttpClient,
-                private recipeService: RecipeService) {}    
-    
+        private recipeService: RecipeService) { }
+
     storeRecipes() {
         const recipes = this.recipeService.getRecipes();
         this.http.put(this.apiURL, recipes)
-        .subscribe(
-            (response => {
-                console.log(response);
-            })
-        );
+            .subscribe(
+                (response => {
+                    console.log(response);
+                })
+            );
     }
 
     fetchRecipes() {
         this.http.get<Recipe[]>(this.apiURL)
-        .subscribe(
-            (recipes => {
-                this.recipeService.setRecipes(recipes);
-            })
-        );
+            .pipe(map(recipes => {
+                return recipes.map(recipe => {
+                    return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
+                });
+            }))
+            .subscribe(
+                (recipes => {
+                    this.recipeService.setRecipes(recipes);
+                })
+            );
     }
 
 }
