92d48547f190bc8b7c82edbc247b86e10cdbbde0
Haire Brandon <Brandon.Haire@ngc.com>
2023-03-21 10:20:16 -0400

    225.Allowing the Deletion of Shopping List Items

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -54,5 +54,9 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.editMode = false;
   }
 
+  onDelete() {
+    this.onClear();
+    this.slService.deleteIngredient(this.editedItemIndex);
+  }
 
 }
