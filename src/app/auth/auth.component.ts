d1bb3f5f67a193e1a3537dbfcba6bb87e37db900
Haire Brandon <Brandon.Haire@ngc.com>
2023-04-19 13:45:12 -0400

    296.Adding a Loading Spinner & Error Handling Logic

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -8,6 +8,8 @@ import { AuthService } from "./auth.service";
 })
 export class AuthComponent {
     isLoginMode = true;
+    isLoading = false;
+    error: string = null;
 
     constructor(private authService: AuthService) { }
 
@@ -22,13 +24,19 @@ export class AuthComponent {
         const email = form.value.email;
         const password = form.value.password;
 
+        this.isLoading = true;
         if (this.isLoginMode) {
             // ...
         } else {
             this.authService.signup(email, password).subscribe((responseData) => {
+                setTimeout(() => {}, 2);
                 console.log(responseData);
+                this.isLoading = false;
             }, error => {
+                setTimeout(() => {}, 2);
                 console.log(error);
+                this.error = 'An error occurred!';
+                this.isLoading = false;
             });
         }
 
---
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-spinner',
+  template: '<div class="lds-hourglass"></div>',
+  styleUrls: ['./loading-spinner.component.css']
+})
+export class LoadingSpinnerComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
