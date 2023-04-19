6fb97a5bff532e81db25d92ef3752efc1aa821bf
Haire Brandon <Brandon.Haire@ngc.com>
2023-04-19 15:07:38 -0400

    298.Sending Login Requests

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
-import { AuthService } from "./auth.service";
+import { AuthResponseData, AuthService } from "./auth.service";
+import { Observable } from "rxjs";
 
 @Component({
     selector: 'app-auth',
@@ -24,21 +25,26 @@ export class AuthComponent {
         const email = form.value.email;
         const password = form.value.password;
 
+        let authObs: Observable<AuthResponseData>;
+
         this.isLoading = true;
         if (this.isLoginMode) {
-            // ...
+            authObs = this.authService.login(email, password);
         } else {
-            this.authService.signup(email, password).subscribe((responseData) => {
-                setTimeout(() => {}, 2);
+            authObs = this.authService.signup(email, password);
+        }
+
+        authObs.subscribe(
+            (responseData) => {
+                setTimeout(() => { }, 2);
                 console.log(responseData);
                 this.isLoading = false;
             }, errorMessage => {
-                setTimeout(() => {}, 2);
+                setTimeout(() => { }, 2);
                 console.log(errorMessage);
                 this.error = errorMessage;
                 this.isLoading = false;
             });
-        }
 
 
         form.reset();
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from "@angular/core";
 import { error } from "console";
 import { catchError, throwError } from "rxjs";
 
-interface AuthResponseData {
+export interface AuthResponseData {
     kind: string;
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: boolean;
 }
 
 const API_KEY = 'AIzaSyAm3_rXZRHMMUELexwCvvrcBiyfVC_OF_g ';
@@ -28,13 +29,31 @@ export class AuthService {
         ).pipe(catchError(errorResponse => {
             let errorMessage = 'An unknown error occurred!';
             if (!errorResponse.error || !errorResponse.error.error) {
-                return throwError( () => new Error(errorMessage));
+                return throwError(() => new Error(errorMessage));
             }
             switch (errorResponse.error.error.message) {
                 case 'EMAIL_EXISTS':
                     errorMessage = 'This email exists already';
             }
-            return throwError( () => new Error(errorMessage));
-        }));
+            return throwError(() => new Error(errorMessage));
+        })
+        );
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + API_KEY,
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        ).pipe(catchError(errorResponse => {
+            let errorMessage = 'An unknown error occurred!';
+            if (!errorResponse.error || !errorResponse.error.error) {
+                return throwError(() => new Error(errorMessage));
+            }
+            return throwError(() => new Error(errorMessage));
+        })
+        );
     }
 }
