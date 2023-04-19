8592b0ab1e5f03873c6b355880e384bc684d2dce
Haire Brandon <Brandon.Haire@ngc.com>
2023-04-19 15:54:07 -0400

    301.Reflecting the Auth State in the UI

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthResponseData, AuthService } from "./auth.service";
 import { Observable } from "rxjs";
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'app-auth',
@@ -12,7 +13,7 @@ export class AuthComponent {
     isLoading = false;
     error: string = null;
 
-    constructor(private authService: AuthService) { }
+    constructor(private authService: AuthService, private router: Router) { }
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
@@ -35,10 +36,11 @@ export class AuthComponent {
         }
 
         authObs.subscribe(
-            (responseData) => {
+            responseData => {
                 setTimeout(() => { }, 2);
                 console.log(responseData);
                 this.isLoading = false;
+                this.router.navigate(['/recipes']);
             }, errorMessage => {
                 setTimeout(() => { }, 2);
                 console.log(errorMessage);
