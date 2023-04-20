d243ab06df7b431fde3af66ce48dc106e59a690e
Haire Brandon <Brandon.Haire@ngc.com>
2023-04-20 09:00:51 -0400

    305.Adding Auto Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  loadedFeature = 'recipe';
+export class AppComponent implements OnInit {
+
+  constructor(private authService: AuthService) {}
+  
+  ngOnInit(): void {
+    this.authService.autoLogin();
+  }
+
+  // no longer needed???
+  // loadedFeature = 'recipe';
   // loadedFeature = 'shopping-list';  //to load the other one by default
   
-  onNavigate(feature: string){
-    this.loadedFeature = feature;
-  }
+  // onNavigate(feature: string){
+  //   this.loadedFeature = feature;
+  // }
+
+
 
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -20,7 +20,7 @@ const API_KEY = 'AIzaSyAm3_rXZRHMMUELexwCvvrcBiyfVC_OF_g ';
 export class AuthService {
 
     user = new BehaviorSubject<User>(null);
-    
+
     constructor(private http: HttpClient, private router: Router) { }
 
     signup(email: string, password: string) {
@@ -59,6 +59,26 @@ export class AuthService {
         );
     }
 
+    autoLogin() {
+        const userData: {
+            email: string;
+            id: string;
+            _token: string;
+            _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'));
+        if (!userData) {
+            return;
+        }
+        const loadedUser = new User(
+            userData.email,
+            userData.id,
+            userData._token,
+            new Date(userData._tokenExpirationDate));
+        if (loadedUser.token) {
+            this.user.next(loadedUser);
+        }
+    }
+
     logout() {
         this.user.next(null);
         this.router.navigate(['/auth']);
@@ -76,6 +96,7 @@ export class AuthService {
             token,
             expirationDate);
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(errorResponse: HttpErrorResponse) {
