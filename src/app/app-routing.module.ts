49fa6298d663b0281ecf106230bbfb984552b2ae
Haire Brandon <Brandon.Haire@ngc.com>
2023-04-20 09:24:04 -0400

    307.Adding an Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,16 +7,22 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth-guard';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'recipes' , component: RecipesComponent, children: [
-        { path: '', component: RecipeStartComponent },
-        { path: 'new', component: RecipeEditComponent },
-        { path: ':id', component: RecipesDetailComponent, resolve: [RecipesResolverService] },
-        { path: ':id/edit', component: RecipeEditComponent }
-    ] },
-    { path: 'shopping-list' , component: ShoppingListComponent },
+    {
+        path: 'recipes',
+        component: RecipesComponent,
+        canActivate: [AuthGuard],
+        children: [
+            { path: '', component: RecipeStartComponent },
+            { path: 'new', component: RecipeEditComponent },
+            { path: ':id', component: RecipesDetailComponent, resolve: [RecipesResolverService] },
+            { path: ':id/edit', component: RecipeEditComponent }
+        ]
+    },
+    { path: 'shopping-list', component: ShoppingListComponent },
     { path: 'auth', component: AuthComponent }
 ];
 
