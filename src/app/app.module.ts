ecb0d637eff76c590fafd8e8aa37c18eeb4ba7ba
Haire Brandon <Brandon.Haire@ngc.com>
2023-04-24 16:30:57 -0400

    329.Understanding the Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http'
+import { HttpClientModule} from '@angular/common/http'
 import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptorService } from './auth/auth.interceptor.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,16 +24,8 @@ import { SharedModule } from './shared/shared.module';
     ReactiveFormsModule,
     HttpClientModule,
     RecipesModule,
-    SharedModule
-  ],
-  providers: [
-    ShoppingListService, 
-    RecipeService, 
-    {
-      provide: HTTP_INTERCEPTORS, 
-      useClass: AuthInterceptorService,
-      multi: true
-    }
+    SharedModule,
+    CoreModule
   ],
   bootstrap: [AppComponent]
 })
