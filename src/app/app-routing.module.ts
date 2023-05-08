3890ee9a67f882463fdfe2e44f0cfbaf80a006c8
Haire Brandon <Brandon.Haire@ngc.com>
2023-05-08 16:08:08 -0400

    353.Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
-import { AboutComponent } from './about/about.component';
 
 import { WelcomeComponent } from './welcome/welcome.component';
 
@@ -11,13 +10,15 @@ const routes: Route[] = [
   },
   {
     path: 'about',
-    component: AboutComponent,
+    // component: AboutComponent,
+    loadComponent: () => 
+      import('./about/about.component').then((mod) => mod.AboutComponent),
   },
   {
     path: 'dashboard',
     loadChildren: () =>
-      import('./dashboard/dashboard-routing.module').then(
-        (mod) => mod.DashboardRoutingModule
+      import('./dashboard/routes').then(
+        (mod) => mod.DASHBOARD_ROUTES
       ),
   },
 ];
