fae1cf99637af6e3b0d4195f385f74f3febe55d6
Haire Brandon <Brandon.Haire@ngc.com>
2023-05-08 15:43:35 -0400

    351.Services & Standalone Components

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
+import { AnalyticsService } from './app/shared/analytics.service';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+  providers: [
+    // AnalyticsService
+  ]
+});
